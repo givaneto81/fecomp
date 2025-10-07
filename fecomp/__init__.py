1b4773e328de838118dd2bdf9cab922a3b672b11
givaneto81 <gron@poli.br>
2025-10-07 07:03:00 -0300

    safety: implementa proteção com csrf para evitar requests externos

--- fecomp/__init__.py
+++ fecomp/__init__.py
@@ -1,31 +1,33 @@
 import os
 import google.generativeai as genai
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 from .config import Config
 from .extensions import db 
+
+csrf = CSRFProtect() 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
+    csrf.init_app(app)
 
     if app.config['GEMINI_API_KEY']:
         genai.configure(api_key=app.config['GEMINI_API_KEY'])
     else:
         print("AVISO: Chave da API do Gemini não encontrada.")
 
-    # Importação dos blueprints com os nomes em português
     from .autenticacao import auth_bp
     from .visoes import views_bp
     from .api import api_bp
-    from .tutorial import tutorial_bp # NOVO BLUEPRINT
+    from .tutorial import tutorial_bp
 
-    # Registo dos blueprints
     app.register_blueprint(auth_bp)
     app.register_blueprint(views_bp)
     app.register_blueprint(api_bp, url_prefix='/api')
-    app.register_blueprint(tutorial_bp) # NOVO BLUEPRINT
+    app.register_blueprint(tutorial_bp)
 
     with app.app_context():
         db.create_all()
